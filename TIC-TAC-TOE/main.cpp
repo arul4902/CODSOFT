fa027e3f59b1b47173caa53666bf3aca3a777b9c
arul4902 <kumarsiva42887@gmail.com>
2023-11-12 20:59:57 +0530

    Initial commit

---
+++ TIC-TAC-TOE/main.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+
+void displayBoard(char board[3][3]) {
+    std::cout << "Game Board:" << std::endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            std::cout << board[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+}
+
+bool isWinner(char board[3][3], char player) {
+    for (int i = 0; i < 3; i++) {
+        if (board[i][0] == player && board[i][1] == player && board[i][2] == player) {
+            return true;
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        if (board[0][i] == player && board[1][i] == player && board[2][i] == player) {
+            return true;
+        }
+    }
+
+    if (board[0][0] == player && board[1][1] == player && board[2][2] == player) {
+        return true;
+    }
+
+    if (board[0][2] == player && board[1][1] == player && board[2][0] == player) {
+        return true;
+    }
+
+    return false;
+}
+
+bool isBoardFull(char board[3][3]) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (board[i][j] == ' ') {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+void playGame() {
+    char board[3][3] = {{' ', ' ', ' '},
+                        {' ', ' ', ' '},
+                        {' ', ' ', ' '}};
+
+    char playerX = 'X';
+    char playerO = 'O';
+    char currentPlayer = playerX;
+    bool isGameWon = false;
+
+    std::cout << "Players: Assign 'X' and 'O' to two players." << std::endl;
+
+    while (!isGameWon && !isBoardFull(board)) {
+        displayBoard(board);
+        int row, col;
+
+        std::cout << "Player " << currentPlayer << ", enter your move (row and column, both 0 to 2): ";
+        std::cin >> row >> col;
+
+        if (row < 0 || row >= 3 || col < 0 || col >= 3 || board[row][col] != ' ') {
+            std::cout << "Invalid move, please try again." << std::endl;
+            continue;
+        }
+
+        board[row][col] = currentPlayer;
+
+  
+        isGameWon = isWinner(board, currentPlayer);
+
+      
+        currentPlayer = (currentPlayer == playerX) ? playerO : playerX;
+    }
+
+    displayBoard(board);
+
+    if (isGameWon) {
+        std::cout << "Player " << currentPlayer << " wins!" << std::endl;
+    } else {
+        std::cout << "It's a draw!" << std::endl;
+    }
+}
+
+int main() {
+    std::cout << "Let the games begin! 🎮✨" << std::endl;
+
+    char playAgain = 'Y';
+    while (playAgain == 'Y' || playAgain == 'y') {
+        playGame();
+
+        std::cout << "Do you want to play again? (Y/N): ";
+        std::cin >> playAgain;
+    }
+
+    std::cout << "Thank you for playing!" << std::endl;
+
+    return 0;
+}
